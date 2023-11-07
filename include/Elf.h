f081825801922876178a1d708b30f53355ced562
Jenperdsmalangue <65119790+Jenperdsmonlat1@users.noreply.github.com>
2023-11-07 13:00:05 +0100

    Add files via upload

---
+++ include/Elf.h
@@ -0,0 +1,111 @@
+
+#ifndef _ELF_
+#define _ELF_
+
+#include <stdint.h>
+#include "ElfType.h"
+
+#define EI_MAG0 0
+#define EI_MAG1 1
+#define EI_MAG2 2
+#define EI_MAG3 3
+#define EI_CLASS 4
+#define EI_DATA 5
+#define EI_VERSION 6
+#define EI_OSABI 7
+#define EI_ABIVERSION 8
+#define EI_PAD 9
+#define EI_NIDENT 16
+
+#define ELFCLASS32 1
+#define ELFCLASS64 2
+
+#define ELFDATA2LSB 1
+#define ELFDATA2MSB 2
+
+#define ELFOSABI_SYSV 0
+#define ELFOSABI_HPUX 1
+#define ELFOSABI_STANDALONE 255
+
+#define ET_NONE 0
+#define ET_REL 1
+#define ET_EXEC 2
+#define ET_DYN 3
+#define ET_CORE 4
+#define ET_LOOS 0xFE00
+#define ET_HIOS 0xFEFF
+#define ET_LOPROC 0xFF00
+#define ET_HIPROC 0xFFFF
+
+#define SHN_UNDEF 0
+#define SHN_LOPROC 0xFF00
+#define SHN_HIPROC 0xFF1F
+#define SHN_LOOS 0xFF20
+#define SHN_HIOS 0xFF3F
+#define SHN_ABS 0xFFF1
+#define SHN_COMMON 0xFFF2
+
+#define SHT_NULL 0
+#define SHT_PROGBITS 1
+#define SHT_SYMTAB 2
+#define STRTAB 3
+#define SHT_RELA 4
+#define SHT_HASH 5
+#define SHT_DYNAMIC 6
+#define SHT_NOTE 7
+#define SHT_NOBITS 8
+#define SHT_REL 9
+#define SHT_SHLIB 10
+#define SHT_DYNSYM 11
+#define SHT_LOOS 0x60000000
+#define SHT_HIOS 0x6FFFFFFF
+#define SHT_LOPROC 0x70000000
+#define SHT_HIPROC 0x7FFFFFFF
+
+#define SHF_WRITE 0x1
+#define SHF_ALLOC 0x2
+#define SHF_EXECINSTR 0x4
+#define SHF_MASKOS 0x0F000000
+#define SHF_MASKPROC 0xF0000000
+
+
+typedef struct {
+    uint8_t e_ident[16];
+    Elf64_Half e_type;
+    Elf64_Half e_machine;
+    Elf64_Word e_version;
+    Elf64_Addr e_entry;
+    Elf64_Off e_phoff;
+    Elf64_Off e_shoff;
+    Elf64_Word e_flags;
+    Elf64_Half e_ehsize;
+    Elf64_Half e_phentsize;
+    Elf64_Half e_phnum;
+    Elf64_Half e_shentsize;
+    Elf64_Half e_shnum;
+    Elf64_Half e_shstrndx;
+} Elf64_Ehdr;
+
+typedef struct {
+    Elf64_Word sh_name;
+    Elf64_Word sh_type;
+    Elf64_XWord sh_flags;
+    Elf64_Addr sh_addr;
+    Elf64_Off sh_offset;
+    Elf64_Xword sh_size;
+    Elf64_Word sh_link;
+    Elf64_Word sh_info;
+    Elf64_Xword sh_addralign;
+    Elf64_Xword sh_entsize;
+} Elf64_Shdr;
+
+typedef struct {
+    Elf64_Word st_name;
+    uint8_t st_info;
+    uint8_t st_other;
+    Elf64_Half st_shndx;
+    Elf64_Addr st_value;
+    Elf64_XWord st_size;
+} Elf64_Sym;
+
+#endif
---
+++ include/ElfType.h
@@ -0,0 +1,15 @@
+
+#ifndef _ELF_TYPE_
+#define _ELF_TYPE_
+
+#include <stdint.h>
+
+typedef uint64_t Elf64_Addr
+typedef uint64_t Elf64_Off
+typedef uint16_t Elf64_Half
+typedef uint32_t Elf64_Word
+typedef uint32_t Elf64_SWord
+typedef uint64_t Elf64_XWord
+typedef uint64_t Elf64_SXWord
+
+#endif
